6bc6ca92484c3ec49576384cacb1caad955f90b5
RACHANA651 <85789960+RACHANA651@users.noreply.github.com>
2022-10-01 03:06:16 +0530

    Update Sumthreeandfive.java

--- com/company/Sumthreeandfive.java
+++ com/company/Sumthreeandfive.java
@@ -4,6 +4,7 @@ public class Sumthreeandfive {
     public static void main(String[] args) {
 
         System.out.println(sumthreeandfive());
+        System.out.println(sumthreeandfive());
 
     }
 

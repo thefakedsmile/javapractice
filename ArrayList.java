b3668cd470905718e6b2e46b52fe59584ec3c92b
vicky kumar <vickyjnv7@gmail.com>
2022-10-01 02:20:06 +0530

    Update ArrayList.java
    Code in provement

--- ArrayList.java
+++ ArrayList.java
@@ -11,6 +11,16 @@ public class ArrayList {
         resizeArray();
 
     }
+    
+    public static void main(String[] args) {
+        System.out.println("enter 200 int");
+        getInput();
+        PrintArray(baseData);
+        resizeArray();
+
+    }
+    
+    
 
     private static void getInput(){
         for(int i=0;i<baseData.length;i++){

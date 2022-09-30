a336c061d97121152a7a57c30978222cc59e4f6c
RACHANA651 <85789960+RACHANA651@users.noreply.github.com>
2022-10-01 03:04:31 +0530

    Update Switch.java

--- com/company/Switch.java
+++ com/company/Switch.java
@@ -28,7 +28,23 @@ public class Switch {
                 System.out.println("was not 1 or 2");
                 break;
         }
+ int switchValue=3;
 
+        switch (switchValue){
+            case 1:
+                System.out.println("value was 1");
+                break;
+            case 2:
+                System.out.println("value was 2");
+                break;
+            case 3: case 4: case 5:
+                System.out.println("was a 3 or 4 or 5");
+                System.out.println("actually it was a "+switchValue);
+                break;
+            default:
+                System.out.println("was not 1 or 2");
+                break;
+        }
 
         char charValue='A';
         switch(charValue){

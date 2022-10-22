1d8dda485b636a66be1023afe2e86cc5b5c9c22e
divyanshujnv <116447130+divyanshujnv@users.noreply.github.com>
2022-10-22 20:13:01 +0530

    Update CharAndBoolean.java

--- com/company/CharAndBoolean.java
+++ com/company/CharAndBoolean.java
@@ -8,6 +8,7 @@ public class CharAndBoolean {
         //unicode cahracter
         char myUnicodeChar='\u00A9';
         System.out.println("uni"+myUnicodeChar);
+         System.out.println("uni"+myUnicodeChar);
 
         boolean myboolean=true;
         System.out.println(myboolean);
